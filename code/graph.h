459c78e504f82a21fecd277eb1c6f0f54168132e
root <mesea7@naver.com>
2022-11-09 22:58:33 +0000

    edges

--- code/graph.h
+++ code/graph.h
@@ -17,27 +17,27 @@ class Graph {
         
         //functions to insert vertices
         void insertVertex(int v, Airport ap);
-        void insertAllVertices(std::string & fileName);
+        void loadVertices(std::string & fileName);
         
         //functions to create and insert edges
-        vector<string> _createEdge(string & line);
+        vector<string> _lineToFlightContents(string & line);
         Flight createEdge(vector<string> flightVector);     
         void insertEdge(Flight f);
-        void insertAllEdge(std::string & fileName);
+        void loadEdges(std::string & fileName);
         
         //functions that return private maps for debugging
         unordered_map<int, Airport> getVertices();
         unordered_map<int, Flight> adjVertWithWeight(int airportID);
         string getAPName(int ID);
         
-        //function to harvest adjmatrix from a graph obj
-        void adjMatrix(PageRank* pr_obj);
+        // //function to harvest adjmatrix from a graph obj
+        // void adjMatrix(PageRank* pr_obj);
 
 
     private:
         //functions that calculate the weight of an edge
         double calcWeight(int fromID, int toID);
-        double radianConvert(double degree);
+        double convertRadian(double degree);
         
         //each graph object hosts a map of airports to its corresponding ID
         unordered_map<int, Airport> vertices;
